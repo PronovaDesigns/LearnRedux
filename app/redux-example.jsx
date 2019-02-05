0d310bd2c726cb31c0dfc0f7a67ae0aa39482b0d
Christopher A. Migliorini <migliorini.christopher@gmail.com>
2019-02-05 17:42:45 -0600

    Add support for CHANGE_NAME action.

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,14 +2,29 @@ var redux = require('redux');
 
 console.log('Starting Redux example');
 
-// Reducer requires default state and to return state.
+// Reducer requires default state and to return state. Responsible for computing new versions of our state.
 var reducer = (state = {name: 'Anonymous'}, action) => {
   // state = state || {name: 'Anonymous'}; // ES5 way of creating default state.
 
-  return state;
+   switch (action.type) {
+     case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+      default:
+        return state;
+   }
 };
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Chris'
+});
+
+console.log('Name should be Chris', store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -8,10 +8,30 @@ var stateDefault = {
     todos: []
 }
 var reducer = (state = {stateDefault}, action) => {
-  return state;
+
+  switch(action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        // ...state is all of the existing state variables and the following argument(s) override the specific existing state variable.
+        ...state,
+        searchText: action.searchText
+      };
+    default:
+      return state;
+  }
 };
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+var action = {
+  // Type is the name of the action.
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'Feed the dog.'
+};
+
+store.dispatch(action);
+
+console.log('searchText should be Feed the dog', store.getState());
